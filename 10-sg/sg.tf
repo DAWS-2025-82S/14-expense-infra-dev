23542fa5952a689cfd0fdb2d1ff987a9974418e7
rajasekhar <raj479.mits@gmail.com>
2025-02-12 05:03:30 +0530

    session-32

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -38,3 +38,36 @@ module "bastion_sg" {
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     common_tags = var.common_tags
 }
+
+module "app_alb_sg" {
+    source = "git::https://github.com/DAWS-2025-82S/12-terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "app-alb"
+    sg_description = "Created for application load(backend) balancer in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+# APP ALB(Backend Load Balancer) accepting traffic from bastion security group instead of IP
+# here we are not  configuring Bastion IP bcoz it may change
+resource "aws_security_group_rule" "app_alb_bastion" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp" 
+  # configuring bastion security group instead of Bastion IP(cidr)
+  source_security_group_id  = module.bastion_sg.sg_id
+  security_group_id = module.app_alb_sg.sg_id
+}
+
+# To acess Bastion instance using public IP
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp" 
+  cidr_blocks = ["49.37.219.75/32"] # Provide your local system IP # This IP is not fixed and it will change
+  security_group_id = module.bastion_sg.sg_id
+}
+
