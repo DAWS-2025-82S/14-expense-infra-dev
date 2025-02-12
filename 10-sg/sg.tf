dabe1d797e320e15a1e0ca1456d24a624845b98f
rajasekhar <raj479.mits@gmail.com>
2025-02-12 23:10:05 +0530

    session-33

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -49,6 +49,7 @@ module "app_alb_sg" {
     common_tags = var.common_tags
 }
 
+# Access to Load Balancer from Bastion(Bastion security group instances)
 # APP ALB(Backend Load Balancer) accepting traffic from bastion security group instead of IP
 # here we are not  configuring Bastion IP bcoz it may change
 resource "aws_security_group_rule" "app_alb_bastion" {
@@ -61,13 +62,89 @@ resource "aws_security_group_rule" "app_alb_bastion" {
   security_group_id = module.app_alb_sg.sg_id
 }
 
-# To acess Bastion instance using public IP
+# To access Bastion instance using public IP from local Laptop
 resource "aws_security_group_rule" "bastion_public" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp" 
-  cidr_blocks = ["49.37.219.75/32"] # Provide your local system IP # This IP is not fixed and it will change
+  #cidr_blocks = ["49.37.219.75/32"] # Provide your local system IP # This IP is not fixed and it will change
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.bastion_sg.sg_id
 }
 
+# Security Group for VPN Server
+# ports 22, 443, 1194, 943 --> VPN ports
+# 22 -- for ssh access
+# 443 -- for access from browser
+# 1194 & 943 -- VPN internal ports
+module "vpn_sg" {
+    source = "git::https://github.com/DAWS-2025-82S/12-terraform-aws-securitygroup.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "vpn"
+    sg_description = "Created for VPN instances in expense dev"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    common_tags = var.common_tags
+}
+
+# Security Rules for VPN
+# ssh port access to connect to VPN server using command below
+# ssh -i privatekey openvpnas@PublicIP
+resource "aws_security_group_rule" "vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_443" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.vpn_sg.sg_id
+}
+
+# Access to Load Balancer from VPN
+# to allow traffic from VPN to app load balancer
+resource "aws_security_group_rule" "app_alb_vpn" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  source_security_group_id = module.vpn_sg.sg_id
+  security_group_id = module.app_alb_sg.sg_id
+}
+
+# Access to Database from Bastion(Bastion security group instances)
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id = module.bastion_sg.sg_id
+  security_group_id = module.mysql_sg.sg_id
+}
